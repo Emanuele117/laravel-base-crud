0e495d136adbb46b16316f373e985687148cfaa6
Emanuele117 <emanuele.pica7@gmail.com>
2022-01-21 16:17:47 +0100

    conferma della cancellazione del post

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -37,11 +37,37 @@
                     
                     <a href="{{route('admin.posts.edit', $post->id)}}">Edit </a>
 
-                    <form action="{{route('admin.posts.destroy', $post->id)}}" method="post">
-                        @csrf 
-                        @method('DELETE')
-                        <button type="submit">Delete</button>
-                    </form>
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-primary btn-lg" data-bs-toggle="modal" data-bs-target="#delete{{$post->id}}">
+                      Elimina
+                    </button>
+                    
+                    <!-- Modal -->
+                    <div class="modal fade" id="delete{{$post->id}}" tabindex="-1" role="dialog" aria-labelledby="modal-{{$post->id}}" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title">Elimina il Post {{$post->title}}</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                   <p>Attenzione! Stai per cancellare definitivamente un post</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
+                                    <form action="{{route('admin.posts.destroy', $post->id)}}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+
+                                        <button type="submit">
+                                            Cancella
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </td>
             </tr>
             @endforeach
